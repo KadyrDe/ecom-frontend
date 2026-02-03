fdbdbc928045f468c43ce54a2a1115207a7300d8
Kadyr Demubay <demubajkadyr@gmail.com>
2026-02-03 18:35:38 +0500

    Initial commit

---
+++ src/utils/constant.js
@@ -0,0 +1,9 @@
+import bannerImageOne from "../assets/sliders/1.jpg";
+import bannerImageTwo from "../assets/sliders/1.jpg";
+import bannerImageThree from "../assets/sliders/1.jpg";
+
+export {
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree
+}
